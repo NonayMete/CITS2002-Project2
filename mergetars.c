38c9f8919d0b5ffef6649f0fb003341e408dd5ba
NonayMete <jakelorkin@hotmail.com>
2020-10-14 15:59:15 +0800

    should work on linux now

--- mergetars.c
+++ mergetars.c
@@ -7,12 +7,12 @@ int main(int argc, char *argv[])
         printf("Incorrect arguments given\n");
         //exit(EXIT_FAILURE);
     }
-    printf("hello World!\n");
-    int pid = fork();
-    printf("PID: %i", getpid());
+    printf("hello World! 1\n");
+    //int pid = fork();
+    //printf("PID: %i", getpid());
     
-    char *args[2] = {"tar -cvf testfromcode.tar Project1", NULL};
-    execvp("tar", args);
+    char *args[] = {"tar", "-cvf", "testfromcode.tar", "Project1",  NULL};
+    execvp(args[0], args);
     cleanup();
     return 0;
 }
