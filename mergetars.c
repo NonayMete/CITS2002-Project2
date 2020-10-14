2a9193e834a976a6dce80bad29102f88a6cd580a
NonayMete <jakelorkin@hotmail.com>
2020-10-14 16:46:46 +0800

    added fork and waiting

--- mergetars.c
+++ mergetars.c
@@ -8,11 +8,22 @@ int main(int argc, char *argv[])
         //exit(EXIT_FAILURE);
     }
     printf("hello World! 1\n");
-    //int pid = fork();
-    //printf("PID: %i", getpid());
-    
-    char *args[] = {"tar", "-cvf", "testfromcode.tar", "Project1",  NULL};
-    execvp(args[0], args);
+
+    pid_t pid = fork();
+
+    if(pid == -1) {
+        printf("didn't fork, error occured\n");
+        exit(EXIT_FAILURE);
+    } else if(pid == 0) {
+        printf("child process running command\n");
+        char *args[] = {"tar", "-cvf", "testfromcode.tar", "Project1",  NULL};
+        execvp(args[0], args);
+    } else {
+        int status;
+        printf("waiting for child process\n");
+        waitpid(pid, &status, 0);
+    }
+
     cleanup();
     return 0;
 }
--- mergetars.h
+++ mergetars.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h> 
+#include <sys/wait.h>
 
 #include "cleanup.c"
 #include "findfiles.c"
